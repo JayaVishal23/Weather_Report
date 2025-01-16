ceb579fbad106cff96b2083c5df865e4c5742979
Jaya Vishal <143166798+JayaVishal23@users.noreply.github.com>
2025-01-16 15:34:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const api_id = "7a441c7d3209d3b69a9430c508bbb870";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const loc = req.body.uloc;
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${api_id}`
+    );
+
+    const data = response.data;
+    //   console.log(data);
+    const descrip = data.weather[0].description;
+    const temp = data.main.temp - 273;
+    const tempC = temp.toFixed(2);
+    const feels = data.main.feels_like - 273;
+    const feelsC = feels.toFixed(2);
+    const wspd = data.wind.speed;
+    res.render("index.ejs", {
+      location: loc,
+      description: descrip,
+      temperature: tempC,
+      feelslike: feelsC,
+      windspeed: wspd,
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log("Listening");
+});
